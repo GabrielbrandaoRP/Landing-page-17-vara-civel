8571791f60f122991a969f8bf9bded7e034c99e1
GabrielbrandaoRP <gabrielhand15@gmail.com>
2024-05-29 11:32:01 -0300

    Personalizacao do footer e mudancas conforme demanda do cliente

--- src/script.js
+++ src/script.js
@@ -1,19 +1,4 @@
-// const accordions = document.querySelectorAll('.accordion');
 
-// accordions.forEach(accordion => {
-//     accordion.addEventListener('click', () => {
-//         const body = accordion.querySelector('.accordionBody');
-//         body.classList.toggle('active');
-
-//         const head = document.querySelector('.accordion');
-//         head.classList.toggle('activeHeaderAccordion');
-
-//         const header = document.querySelector('.accordionHeader');
-//         header.classList.toggle('activeHeaderAccordion');
-
-       
-//     })
-// })
 
 
 let backButton = document.getElementById("backBtn");
