c08139af299913e1ac791d04ca94cc81789e2123
GabrielbrandaoRP <gabrielhand15@gmail.com>
2024-05-28 21:40:54 -0300

    Layout inicial

--- src/script.js
+++ src/script.js
@@ -1,16 +1,36 @@
-const accordions = document.querySelectorAll('.accordion');
+// const accordions = document.querySelectorAll('.accordion');
 
-accordions.forEach(accordion => {
-    accordion.addEventListener('click', () => {
-        const body = accordion.querySelector('.accordionBody');
-        body.classList.toggle('active');
+// accordions.forEach(accordion => {
+//     accordion.addEventListener('click', () => {
+//         const body = accordion.querySelector('.accordionBody');
+//         body.classList.toggle('active');
 
-        const head = document.querySelector('.accordion');
-        head.classList.toggle('activeHeaderAccordion');
+//         const head = document.querySelector('.accordion');
+//         head.classList.toggle('activeHeaderAccordion');
 
-        const header = document.querySelector('.accordionHeader');
-        header.classList.toggle('activeHeaderAccordion');
+//         const header = document.querySelector('.accordionHeader');
+//         header.classList.toggle('activeHeaderAccordion');
 
        
-    })
-})
+//     })
+// })
+
+
+let backButton = document.getElementById("backBtn");
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backButton.style.display = "block";
+    } else
+        backButton.style.display = "block";
+}
+
+
+backButton.onclick = function () {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
